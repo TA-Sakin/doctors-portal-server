4dda087f48bd35b5ec88c8efff859b0ee6d4f246
TA-Sakin <tasakin98@gmail.com>
2022-05-25 21:20:15 +0600

    payment, sending email and other

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const jwt = require("jsonwebtoken");
-const nodemailer = require('nodemailer');
-const mg = require('nodemailer-mailgun-transport');
+const nodemailer = require("nodemailer");
+const mg = require("nodemailer-mailgun-transport");
 const port = process.env.PORT || 5000;
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 app.use(express.json());
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uwnmc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -22,26 +17,26 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-function verifyJWT(req,res,next){
-  const authHeader = req.headers.authorization
-  if(!authHeader){
-    res.status(401).send({message: 'Unauthorized access'});
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.status(401).send({ message: "Unauthorized access" });
   }
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
-      if(err){
-        return res.status(403).send({message: 'Forbidden access'})
-      }
-      req.decoded = decoded
-      next()
-    });
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
 }
 
-function sendAppointmentEmail(booking){
-  const {patient, patientName, treatment, date, slot} = booking;
+function sendAppointmentEmail(booking) {
+  const { patient, patientName, treatment, date, slot } = booking;
   const email = {
-    from: 'myemail@example.com',
-    to: 'altahjib10@gmail.com',
+    from: "myemail@example.com",
+    to: "altahjib10@gmail.com",
     subject: `Your appointment for ${treatment} is on ${date} at ${slot} is confirmed`,
     html: `
     <div>
@@ -53,13 +48,12 @@ function sendAppointmentEmail(booking){
     <a href="https://github.com/TA-Sakin">unsubscribe</a>
     </div>
     `,
-    text: `Your appointment for ${treatment} is on ${date} at ${slot} is confirmed`
-  }
+    text: `Your appointment for ${treatment} is on ${date} at ${slot} is confirmed`,
+  };
   nodemailerMailgun.sendMail(email, (err, info) => {
     if (err) {
       console.log(err);
-    }
-    else {
+    } else {
       console.log(info);
     }
   });
@@ -67,10 +61,37 @@ function sendAppointmentEmail(booking){
 const auth = {
   auth: {
     api_key: process.env.EMAIL_SENDER_KEY,
-    domain: 'sandboxd96939b91c8047c2acb2afcd3f91dcf4.mailgun.org'
-  }
-}
+    domain: "sandboxd96939b91c8047c2acb2afcd3f91dcf4.mailgun.org",
+  },
+};
 const nodemailerMailgun = nodemailer.createTransport(mg(auth));
+
+function sendPaymentConfirmationEmail(booking) {
+  const { patient, patientName, treatment, date, slot } = booking;
+  const email = {
+    from: "myemail@example.com",
+    to: "altahjib10@gmail.com",
+    subject: `We have received your payment for ${treatment} on ${date} at ${slot}.`,
+    html: `
+    <div>
+    <p>Hello, ${patientName}</p>
+    <h3>Thank you for your payment.</h3>
+    <p>Looking forward to seeing you on ${date} at ${slot}.</p>
+    <h3>Our address</h3>
+    <p>Chittagong,Bangladesh</p>
+    <a href="https://github.com/TA-Sakin">unsubscribe</a>
+    </div>
+    `,
+    text: `Your payment for appointment ${treatment} on ${date} at ${slot} is confirmed`,
+  };
+  nodemailerMailgun.sendMail(email, (err, info) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+}
 
 async function run() {
   try {
@@ -79,41 +100,82 @@ async function run() {
     const bookingCollection = client.db("doctorsPortal").collection("booking");
     const userCollection = client.db("doctorsPortal").collection("users");
     const doctorsCollection = client.db("doctorsPortal").collection("doctors");
+    const paymentsCollection = client
+      .db("doctorsPortal")
+      .collection("payments");
 
-    const verifyAdmin = async (req,res,next)=>{
+    const verifyAdmin = async (req, res, next) => {
       const requestor = req.decoded.email;
-          const requestorAccount = await userCollection.findOne({email:requestor})
-          if(requestorAccount.role === 'admin'){
-            next()
-          }
-          else{
-            res.status(403).send({message: 'forbidden'})
-          }
-    }
+      const requestorAccount = await userCollection.findOne({
+        email: requestor,
+      });
+      if (requestorAccount.role === "admin") {
+        next();
+      } else {
+        res.status(403).send({ message: "Forbidden access" });
+      }
+    };
+    app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+      const service = req.body;
+      const price = service.price;
+      const amount = price * 100;
+      // Create a PaymentIntent with the order amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
+    });
 
     app.get("/service", async (req, res) => {
       const query = {};
-      const cursor = serviceCollection.find(query).project({name:1});
+      const cursor = serviceCollection.find(query).project({ name: 1 });
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.get("/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const appointment = await bookingCollection.findOne(query);
+      res.send(appointment);
+    });
+    app.patch("/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const payment = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId,
+        },
+      };
+      const result = await paymentsCollection.insertOne(payment);
+      const updateBooking = await bookingCollection.updateOne(
+        filter,
+        updatedDoc
+      );
+      res.send(updatedDoc);
+    });
+
     app.get("/user", verifyJWT, async (req, res) => {
       const users = await userCollection.find().toArray();
       res.send(users);
     });
+
     app.get("/booking", verifyJWT, async (req, res) => {
       const patient = req.query.patient;
-      const decodedEmail = req.decoded.email
-      if(patient === decodedEmail){
+      const decodedEmail = req.decoded.email;
+      if (patient === decodedEmail) {
         const query = { patient: patient };
         const cursor = bookingCollection.find(query);
         const result = await cursor.toArray();
         return res.send(result);
-      }
-      else{
-        return res.status(403).send({message: 'Forbidden access'})
+      } else {
+        return res.status(403).send({ message: "Forbidden access" });
       }
     });
+
     app.post("/booking", async (req, res) => {
       const booking = req.body;
       const query = {
@@ -126,7 +188,7 @@ async function run() {
         return res.send({ success: false, booking: exists });
       }
       const result = await bookingCollection.insertOne(booking);
-      sendAppointmentEmail( booking)
+      sendAppointmentEmail(booking);
       res.send({ success: true, result });
     });
 
@@ -162,19 +224,19 @@ async function run() {
 
     app.put("/user/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
-        const filter = { email: email };
-        const updateDoc = {
-          $set: {role: 'admin'},
-        };
-        const result = await userCollection.updateOne(filter, updateDoc);
-        res.send(result);
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
     });
-    app.get('/admin/:email', async(req,res)=>{
+    app.get("/admin/:email", async (req, res) => {
       const email = req.params.email;
-      const user = await userCollection.findOne({email:email})
-      const isAdmin = user.role === 'admin'
-      res.send({admin: isAdmin})
-    })
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
@@ -189,14 +251,14 @@ async function run() {
         process.env.ACCESS_TOKEN_SECRET,
         { expiresIn: "1h" }
       );
-      res.send({result, token});
+      res.send({ result, token });
     });
 
-    app.post('/doctors', verifyJWT, verifyAdmin, async(req,res)=>{
+    app.post("/doctors", verifyJWT, verifyAdmin, async (req, res) => {
       const doctors = req.body;
-      const result = await doctorsCollection.insertOne(doctors)
-      res.send(result)
-    })
+      const result = await doctorsCollection.insertOne(doctors);
+      res.send(result);
+    });
 
     app.get("/doctors", verifyJWT, verifyAdmin, async (req, res) => {
       const doctors = await doctorsCollection.find().toArray();
@@ -204,7 +266,7 @@ async function run() {
     });
     app.delete("/doctor/:email", verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
-      const filter = {email: email}
+      const filter = { email: email };
       const result = await doctorsCollection.deleteOne(filter);
       res.send(result);
     });
